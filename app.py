1db3c03c4cb104735c213529ded1d240f6c391c4
Eric Zhou <ericzhou@Erics-MacBook-Air.local>
2022-11-23 13:32:02 -0500

    server api test

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<p>This is the home for the website</p>"
+@app.route("/test", methods=['GET'])
+def recommendations():
+    app.logger.warning('whattt')
+    if request.method == 'GET':
+        data = {
+            'name': 'eric',
+            'age': 20,
+        }
+        app.logger.warning('testing warning log')
+        return jsonify(data)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
